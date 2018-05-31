c30f424326fcb7f0b7ea3ed681be15a9fbfe0c45
Sreekar Reddy Singareddy <sreekar.singareddy@gmail.com>
2018-05-31 17:45:36 +0530

    Added all Android Studio project files

---
+++ app/src/main/java/com/example/apple/interninternational/LaunchingActivity.java
@@ -0,0 +1,13 @@
+package com.example.apple.interninternational;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launching);
+    }
+}
